940eade032e178de04266d2eb0b7c51d75f304a4
akoshegel <hegelakos@codingsans.com>
2019-12-04 10:12:01 +0100

    fix

--- smarthome-server/index.js
+++ smarthome-server/index.js
@@ -3,7 +3,7 @@ const SerialComminucator = require("./serial-communicator");
 console.log("reading data from serial port...");
 
 const serialComminucator = new SerialComminucator({
-  device: "/tty/USB0"
+  device: "/ttyUSB0"
 });
 
 serialComminucator.init();
